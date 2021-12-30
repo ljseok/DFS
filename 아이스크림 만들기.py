37eb1f51761fd906a564d9007cf3138f163dbe1d
ccccljs <cho38suk@hamail.net>
2021-12-30 10:59:32 +0900

    음료수 얼리기

--- 아이스크림 만들기.py
+++ 아이스크림 만들기.py
@@ -1,36 +1,33 @@
-n=14
-m=15 # map(int, input().split())  n,m을 공백으로 입력받기
-graph = [[0,0,0,0,0,1,1,1,1,0,0,0,0,0],[1,1,1,1,1,1,0,1,1,1,1,1,1,0],[1,1,0,1,1,1,0,1,1,0,1,1,1,0],[1,1,0,1,1,1,0,1,1,0,0,0,0,0],
-         [1,1,0,1,1,1,1,1,1,1,1,1,1,1],[1,1,0,1,1,1,1,1,1,1,1,1,0,0],[1,1,0,0,0,0,0,0,0,1,1,1,1,1],[0,1,1,1,1,1,1,1,1,1,1,1,1,1],
-         [0,0,0,0,0,0,0,0,0,1,1,1,1,1],[0,1,1,1,1,1,1,1,1,1,1,0,0,0],[0,0,0,1,1,1,1,1,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,1,1,0,0,0],
-         [1,1,1,1,1,1,1,1,1,1,0,0,1,1,],[1,1,1,0,0,0,1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,1,1,1,1,1,1,1,1]]
-'''
+n,m = map(int, input().split()) # 세로의 길이 n과 가로의 길이 m 입력
+
+graph = []
 for i in range(n):
-    graph.append(list(map(int, input())))  # 2차원 리스트 입력받기
-'''
+    graph.append(list(map(int, input()))) # 2차원 리스트이 맵정보 입력
 
-def dfs(x, y):  # 연결된 노드들 방문
-
-    if x <= -1 or x >= n or y <= -1 or y >= n:  # 범위를 벗어나는 경우에는 종료
+def dfs(x,y):
+    if x <= -1 or x >= n or y <= -1 or y >= m: # 범위를 벗어나는 경우
         return False
-    if graph[x][y] == 0:  # 노드를 방문하지 않았다면
-        graph[x][y] = 1  # 방문처리
+    if graph[x][y] == 0: # 방문하지 않았을경우
+        graph[x][y] = 1 # 방문처리
 
-        dfs(x - 1, y)  # 상
-        dfs(x, y - 1)  # 좌
-        dfs(x + 1, y)  # 하
-        dfs(x, y + 1)  # 우
+        dfs(x-1,y)
+        dfs(x,y-1)
+        dfs(x+1,y)
+        dfs(x,y+1)
 
         return True
     return False
-
+        # 상하좌우 호출
 
 result = 0
-for i in range(n):  # 모든 위치에서 음료수 채우기
-    for j in range(m):
 
-        if dfs(i, j) == True:  # DFS 실행
-            result += 1
+# 모든 위치에 대해서 음료수 채우고
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True: # true면
+            result += 1 # result값 증가
 print(result)
+
+
 
 
